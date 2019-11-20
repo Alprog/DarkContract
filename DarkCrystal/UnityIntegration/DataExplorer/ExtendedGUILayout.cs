21459496238416c3038e74f8efeab85cb8f92db1
Alexander Tuzhik <alprog@hotmail.com>
2019-11-20 02:18:42 +0200

    cleanup

--- DarkCrystal/UnityIntegration/DataExplorer/ExtendedGUILayout.cs
+++ DarkCrystal/UnityIntegration/DataExplorer/ExtendedGUILayout.cs
@@ -12,28 +12,6 @@ namespace DarkCrystal.UnityIntegration
 {
     public partial class ExtendedGUILayout : GUILayout
     {
-        private const float FButtonWidth = 14.0f;
-        
-        public static bool Button(GUIContent guiContent, System.Action action, params GUILayoutOption[] options)
-        {
-            if (GUILayout.Button(guiContent, options))
-            {
-                action();
-                return true;
-            }
-            return false;
-        }
-
-        public static bool Button(GUIContent guiContent, System.Action action, GUIStyle style, params GUILayoutOption[] options)
-        {
-            if (GUILayout.Button(guiContent, style, options))
-            {
-                action();
-                return true;
-            }
-            return false;
-        }
-
         public static bool Button(string name, System.Action action, params GUILayoutOption[] options)
         {
             if (GUILayout.Button(name, options))
@@ -44,16 +22,6 @@ namespace DarkCrystal.UnityIntegration
             return false;
         }
 
-        public static bool Button(string name, System.Action action, GUIStyle style, params GUILayoutOption[] options)
-        {
-            if (GUILayout.Button(name, style, options))
-            {
-                action();
-                return true;
-            }
-            return false;
-        }
-        
         public static void ReadonlyTextField(string label, string text, params GUILayoutOption[] options)
         {
             var backgrounColor = GUI.backgroundColor;
@@ -62,67 +30,9 @@ namespace DarkCrystal.UnityIntegration
             GUI.backgroundColor = backgrounColor;
         }
 
-        public static bool Toggle(string label, ref bool value, params GUILayoutOption[] options)
-        {
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField(label, GUIStyles.RightAlignment, GUILayout.Width(EditorGUIUtility.labelWidth - 12));
-            var newValue = EditorGUILayout.Toggle(value, options);
-            EditorGUILayout.EndHorizontal();
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-        
-        public static bool ToggleLeft(string label, ref bool value, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.ToggleLeft(label, value, options);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
-        public static bool ToggleLeft(string label, bool value, Action<bool> onValueChanged, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.ToggleLeft(label, value, options);
-            if (value != newValue)
-            {
-                onValueChanged(newValue);
-                return true;
-            }
-            return false;
-        }
-        
-        public static bool Vector2Field(string label, ref Vector2 value)
-        {
-            var newValue = EditorGUILayout.Vector2Field(label, value);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
         public static bool Vector3Field(string label, ref Vector3 value)
         {
             var newValue = EditorGUILayout.Vector3Field(label, value);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-        
-        public static bool Vector2IntField(string label, ref Vector2Int value, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.Vector2IntField(label, value, options);
             if (value != newValue)
             {
                 value = newValue;
@@ -148,58 +58,6 @@ namespace DarkCrystal.UnityIntegration
             return false;
         }
 
-        public static bool TextField(ref string value, GUIStyle style, params GUILayoutOption[] options)
-        {
-            var newValue = GUILayout.TextField(value, style, options);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-        
-        public static bool IntField(string caption, ref int value, int startFrom = 0, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.IntField(caption, value, options);
-            if (value != newValue || value < startFrom)
-            {
-                if (newValue < startFrom)
-                {
-                    newValue = startFrom;
-                }
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
-        public static bool IntField(string caption, ref int value, GUIStyle guiStyle, int startFrom = 0, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.IntField(caption, value, guiStyle, options);
-            if (value != newValue || value < startFrom)
-            {
-                if (newValue < startFrom)
-                {
-                    newValue = startFrom;
-                }
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
-        public static bool IntField(ref int value, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.IntField(value, options);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-        
         public static bool IntField(string name, ref int value, params GUILayoutOption[] options)
         {
             var newValue = EditorGUILayout.IntField(name, value, options);
@@ -210,64 +68,7 @@ namespace DarkCrystal.UnityIntegration
             }
             return false;
         }
-
-        public static bool FloatField(ref float value, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.FloatField(value, options);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
-        public static bool FloatField(string name, ref float value, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.FloatField(name, value, options);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
-        public static bool FloatField(string name, ref float value, GUIStyle guiStyle, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.FloatField(name, value, guiStyle, options);
-            if (value != newValue)
-            {
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
-        public static bool FloatField(string caption, ref float value, float startFrom = 0, params GUILayoutOption[] options)
-        {
-            var newValue = EditorGUILayout.FloatField(caption, value, options);
-            if (value != newValue || value < startFrom)
-            {
-                if (newValue < startFrom)
-                {
-                    newValue = startFrom;
-                }
-                value = newValue;
-                return true;
-            }
-            return false;
-        }
-
-        public static void FoldButton(ref bool foldOut)
-        {
-            var text = foldOut ? "▼" : "►";
-            if (Layout.Button(text, UnityEngine.GUI.skin.label))
-            {
-                foldOut = !foldOut;
-            }
-        }
-
+        
         public static bool FoldLine(bool foldOut, Color guiColor, string caption = null, Texture2D icon = null, GUIStyle panelStyle = null, GUIStyle labelStyle = null)
         {
             var defaultGUIColor = GUI.color;
@@ -331,18 +132,6 @@ namespace DarkCrystal.UnityIntegration
         {
             EndVertical();
             EndHorizontal();
-        }
-
-        public static void HorizontalLine()
-        {
-            EditorGUILayout.LabelField(String.Empty, GUI.skin.horizontalSlider);
-        }
-
-        public static void HorizontalLine(float topMargin, float borMargin)
-        {
-            Layout.Space(topMargin);
-            EditorGUILayout.LabelField(String.Empty, GUI.skin.horizontalSlider);
-            Layout.Space(borMargin);
         }
     }
 
